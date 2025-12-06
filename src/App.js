6f89142fe9cbe50a1ee4a272d4a29d39c2d43597
wnsudwnsudwnsud9999 <wndnsud121213@gmail.com>
2025-12-06 20:29:22 +0900

    Wrap app with AuthProvider and protect main routes

--- src/App.js
+++ src/App.js
@@ -9,23 +9,91 @@ import HistoryPage from "./pages/HistoryPage";
 import CertDetailPage from "./pages/CertDetailPage";
 import StatsPage from "./pages/StatsPage";
 import StudyCalendarPage from "./pages/StudyCalendarPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
 import Layout from "./components/Layout";
+import { AuthProvider } from "./auth/AuthContext";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 export default function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/select" element={<CertSelectPage />} />
-          <Route path="/goal" element={<GoalSettingPage />} />
-          <Route path="/recommend" element={<RecommendPage />} />
-          <Route path="/history" element={<HistoryPage />} />
-          <Route path="/cert/:certId" element={<CertDetailPage />} />
-          <Route path="/stats" element={<StatsPage />} />
-          <Route path="/calendar" element={<StudyCalendarPage />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            {/* 공개 페이지 */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+
+            {/* 로그인 필요한 페이지들 */}
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/select"
+              element={
+                <ProtectedRoute>
+                  <CertSelectPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/goal"
+              element={
+                <ProtectedRoute>
+                  <GoalSettingPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/recommend"
+              element={
+                <ProtectedRoute>
+                  <RecommendPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/history"
+              element={
+                <ProtectedRoute>
+                  <HistoryPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/cert/:certId"
+              element={
+                <ProtectedRoute>
+                  <CertDetailPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/stats"
+              element={
+                <ProtectedRoute>
+                  <StatsPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/calendar"
+              element={
+                <ProtectedRoute>
+                  <StudyCalendarPage />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Layout>
+      </Router>
+    </AuthProvider>
   );
 }
