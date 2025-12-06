819dfa0dcca1950e7106da13361427ed3938d2b9
wnsudwnsudwnsud9999 <wndnsud121213@gmail.com>
2025-12-06 15:08:23 +0900

    Add history route to App component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import CertSelectPage from "./pages/CertSelectPage";
 import GoalSettingPage from "./pages/GoalSettingPage";
 import RecommendPage from "./pages/RecommendPage";
+import HistoryPage from "./pages/HistoryPage";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/select" element={<CertSelectPage />} />
         <Route path="/goal" element={<GoalSettingPage />} />
         <Route path="/recommend" element={<RecommendPage />} />
+        <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </Router>
   );
