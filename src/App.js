0b1a7d0b6c593aa70ab89baaaf2a07d846f82558
wnsudwnsudwnsud9999 <wndnsud121213@gmail.com>
2025-12-06 20:01:00 +0900

    Wrap routes with Layout including Header and Footer

--- src/App.js
+++ src/App.js
@@ -9,20 +9,23 @@ import HistoryPage from "./pages/HistoryPage";
 import CertDetailPage from "./pages/CertDetailPage";
 import StatsPage from "./pages/StatsPage";
 import StudyCalendarPage from "./pages/StudyCalendarPage";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/select" element={<CertSelectPage />} />
-        <Route path="/goal" element={<GoalSettingPage />} />
-        <Route path="/recommend" element={<RecommendPage />} />
-        <Route path="/history" element={<HistoryPage />} />
-        <Route path="/cert/:certId" element={<CertDetailPage />} />
-        <Route path="/stats" element={<StatsPage />} />
-        <Route path="/calendar" element={<StudyCalendarPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/select" element={<CertSelectPage />} />
+          <Route path="/goal" element={<GoalSettingPage />} />
+          <Route path="/recommend" element={<RecommendPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/cert/:certId" element={<CertDetailPage />} />
+          <Route path="/stats" element={<StatsPage />} />
+          <Route path="/calendar" element={<StudyCalendarPage />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
